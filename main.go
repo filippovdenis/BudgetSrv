1caac17636a05ed5b6fb2e09423f4edd5ac90ef4
Denis Filippov <dfilippov@oneincsystems.com>
2019-03-01 12:09:55 +0700

    iniital commit

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gorilla/mux"
+	"net/http"
+	"encoding/json"
+	"log"
+	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
+)
+
+var database *gorm.DB
+
+
+
+type SMSMessage struct {
+	MessageId int64 `gorm:"primary_key";"AUTO_INCREMENT"`
+	MessageBody string
+	DateStr string
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request){
+	println("Method Get")
+	vars := mux.Vars(r)
+	id := vars["id"]
+	entity := vars["entity"]
+	println("entity = ", entity)
+	println("id = ", id)
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request){
+	println("Method Post")
+	vars := mux.Vars(r)
+	id := vars["id"]
+	entity := vars["entity"]
+	println("entity = ", entity)
+	println("id = ", id)
+}
+
+func putHandler(w http.ResponseWriter, r *http.Request){
+	println("Method Put")
+	vars := mux.Vars(r)
+	var EntityVar interface{}
+
+	entity := vars["entity"]
+	println("entity = ", entity)
+	dec := json.NewDecoder(r.Body)
+	switch entity {
+	case "category":
+		cat := Category{}
+		dec.Decode(&cat)
+		EntityVar = cat
+		println("in category case")
+		fmt.Printf ("%T: %v\n", EntityVar, EntityVar)
+		database.Create(&cat)
+	case "smsmessage":
+		EntityVar = SMSMessage{}
+		println("in sms case")
+	default:
+		EntityVar = nil
+	}
+
+
+	if EntityVar == nil {
+		println("nil")
+	}
+}
+
+func main(){
+	fmt.Println("hello")
+
+	connStr := "user=postgres password=tamplier dbname=TestDB sslmode=disable"
+
+	db, err := gorm.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	database = db
+	defer db.Close()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/{entity}/{id:[0-9]+}", getHandler).Methods("GET")
+	router.HandleFunc("/{entity}/{id:[0-9]+}", postHandler).Methods("POST")
+	router.HandleFunc("/{entity}", putHandler).Methods("PUT")
+	http.Handle("/", router)
+	http.ListenAndServe(":8081", nil)
+}
